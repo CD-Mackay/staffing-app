f4aeb996acc88e14a8f3ce3b31f25c67baeb4d6d
Connor Mackay <connor.mackay@gmail.com>
2022-05-20 14:45:30 -0400

    render staff data on index

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// Asset Imports
+import staff from "../src/utilities/dummy-data";
 
 function App() {
   return (
     <div className="App">
-      I am App
+      <ul>
+        {staff.map((employee) => {
+          return <li>{employee.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
