a86f8df70df366877a1428ced8d7a786125d269e
Connor Mackay <connor.mackay@gmail.com>
2022-05-24 11:44:39 -0400

    Add state to input

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 import StyledSearchBar from "./StyledSearchBar";
 
 // Component Imports
 
 const SearchBar = () => {
+  const [parameter, setParameter] = useState("");
+
+  const onChange = (event) => {
+    setParameter(event.target.value);
+  };
   return (
     <StyledSearchBar>
-      <input type="text" placeholder="Search for something!" />
+      <input type="text" value={parameter} onChange={onChange} placeholder="Search for something!" />
     </StyledSearchBar>
   );
 };
