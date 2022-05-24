495b1ad8505d164f80e577a16560ba5d46e19673
Connor Mackay <connor.mackay@gmail.com>
2022-05-24 12:29:29 -0400

    Add further app tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import staff from './utilities/dummy-data';
 
 test('renders the search bar', () => {
   render(<App />);
-  // const linkElement = screen.getByPlaceHolderText(/Search for something/i);
   const searchElement = screen.getByPlaceholderText(/Search for something/i);
   expect(searchElement).toBeInTheDocument();
 });
+
+test('renders the stafflist component', () => {
+  render(<App />);
+  const staffList = screen.getByTestId("staff-list");
+  expect(staffList).toBeInTheDocument();
+})
