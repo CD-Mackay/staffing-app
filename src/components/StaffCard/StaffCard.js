07f8bfc8d022f6965b307802ced06fe0043b57ec
Connor Mackay <connor.mackay@gmail.com>
2022-05-26 15:50:00 -0400

    style employee skilllist

--- src/components/StaffCard/StaffCard.js
+++ src/components/StaffCard/StaffCard.js
@@ -8,34 +8,30 @@ const StaffCard = ({ employee }) => {
   const [employeeSkills, setEmployeeSkills] = useState(skills);
 
   const handleAddSkill = (input) => {
-    console.log("adding new skill")
+    console.log("adding new skill");
     let skillsArray = [...employeeSkills];
     skillsArray.push(input);
     setEmployeeSkills(skillsArray);
   };
 
-  // useEffect(() => {
-
-  // }, [employeeSkills]);
-
   return (
     <StyledStaffCard>
-        <div className="image-wrapper">
-          <img src="images/ongo-gablogian.jpg" alt="ongo gablogian" />
-        </div>
-        <div className="employee-info">
-          <h4>name: {name}</h4>
-          <p>Department: {department}</p>
-        </div>
-        <div className="employee-info">
-          <p>Skills:</p>
+      <div className="image-wrapper">
+        <img src="images/ongo-gablogian.jpg" alt="ongo gablogian" />
+      </div>
+      <div className="employee-info">
+        <h4>name: {name}</h4>
+        <p>Department: {department}</p>
+      </div>
+      <div className="employee-info">
+        <p>Skills:</p>
         <ul>
           {employeeSkills.map((skill) => {
             return <li key={skill}>{skill}</li>;
           })}
         </ul>
+        <CustomInput employeeId={id} onSubmit={handleAddSkill} />
       </div>
-      <CustomInput employeeId={id} onSubmit={handleAddSkill}  />
     </StyledStaffCard>
   );
 };
