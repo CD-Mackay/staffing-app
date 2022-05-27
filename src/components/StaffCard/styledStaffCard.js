38499c0789f91e40a4779c01034a0a2f870e07bd
Connor Mackay <connor.mackay@gmail.com>
2022-05-27 13:47:18 -0400

    pass flag prop to staffcard and children

--- src/components/StaffCard/styledStaffCard.js
+++ src/components/StaffCard/styledStaffCard.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 const StyledStaffCard = styled.div`
+background-color: ${props => props.flag === "green" ? "green" : props.flag === "red" ? "red" : ""};
   display: grid;
   grid-template-columns: 1fr 3fr;
   grid-template-rows: auto;
