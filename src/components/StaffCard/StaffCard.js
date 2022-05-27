06fd7b71a13c7dba0de4d987fc70c4154073682d
Connor Mackay <connor.mackay@gmail.com>
2022-05-27 14:19:33 -0400

    Refactor stafflist styling to match new searchbar

--- src/components/StaffCard/StaffCard.js
+++ src/components/StaffCard/StaffCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import FlagButton from "../FlagButton/FlagButton";
 import CustomInput from "../Input/CustomInput";
 
