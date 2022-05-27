7b4d11e7cd9dd5caa8f3577e57c821c371840c34
Connor Mackay <connor.mackay@gmail.com>
2022-05-27 13:49:07 -0400

    style flagged state on staffcard component

--- src/components/StaffCard/styledStaffCard.js
+++ src/components/StaffCard/styledStaffCard.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const StyledStaffCard = styled.div`
-background-color: ${props => props.flag === "green" ? "green" : props.flag === "red" ? "red" : ""};
+background-color: ${props => props.flag === "green" ? "#BDF0C9" : props.flag === "red" ? "#F0C9CD" : ""};
   display: grid;
   grid-template-columns: 1fr 3fr;
   grid-template-rows: auto;
