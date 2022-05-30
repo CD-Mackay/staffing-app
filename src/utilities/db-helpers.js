703971ce6784c22fca4f9170e70a94ddaae90cbd
Connor Mackay <connor.mackay@gmail.com>
2022-05-30 16:44:56 -0400

    Refactor db helpers

--- src/utilities/db-helpers.js
+++ src/utilities/db-helpers.js
@@ -1,28 +1,17 @@
-import { MongoClient } from "mongodb";
+export async function getAllEvents() {
+  const response = await fetch(' https://staffing-app-8610f-default-rtdb.firebaseio.com/staff.json');
+  const data = await response.json();
 
-async function updateDataBase(req, res) {
+  const events = [];
 
-
-  const connectionString = `mongodb+srv://ConnorTwo:ConnorPass@authcluster.jdoeb.mongodb.net/?retryWrites=true&w=majority`;
-  let client;
-  try {
-    client = await MongoClient.connect(connectionString);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key]
+    });
   }
 
-  const db = client.db();
-  let result;
-  try {
-    result = await db.collection("employees").insertOne("staff");
-  } catch (error) {
-    client.close();
-    res.status(500).json({ message: error.message });
-    return;
-  }
-
-  client.close();
-  res
-    .status(201)
-    .json({ message: "sweeet sweet backend success", message: result });
+  return events;
 };
+
+
