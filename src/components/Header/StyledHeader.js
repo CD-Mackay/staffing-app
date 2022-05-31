bbd0fbc644c9bd404f48c84e1b3567346a51c1bb
Connor Mackay <connor.mackay@gmail.com>
2022-05-31 15:21:39 -0400

    Style nav section

--- src/components/Header/StyledHeader.js
+++ src/components/Header/StyledHeader.js
@@ -10,6 +10,13 @@ border: 1px solid gray;
 background-color: white;
 .nav {
   display: flex;
+  padding: 5px;
+  p {
+    padding: 5px;
+    margin: 2px;
+    border: 1px solid gray;
+    border-radius: 10px;
+  }
 }
 `;
 
