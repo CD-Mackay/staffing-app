1a4e70703309d53285aa1523a3664324b1539147
Connor Mackay <connor.mackay@gmail.com>
2022-06-01 16:57:05 -0400

    build add employee form component

---
+++ src/components/AddEmployee/AddEmployee.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import StyledAddEmployee from "./StyledAddEmployee";
+
+const AddEmployee = () => {
+  return (
+    <StyledAddEmployee>
+      <p>G'day, I'm addemployee</p>
+    </StyledAddEmployee>
+  );
+};
+
+export default AddEmployee;
