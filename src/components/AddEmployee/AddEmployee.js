a7e4cbe65573fb88f225c676cc46c9bca6d773dd
Connor Mackay <connor.mackay@gmail.com>
2022-06-03 14:32:43 -0400

    Add refs to form input

--- src/components/AddEmployee/AddEmployee.js
+++ src/components/AddEmployee/AddEmployee.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import StyledAddEmployee from "./StyledAddEmployee";
 
 export default function AddEmployee() {
+  const nameInputRef = useRef();
+  const titleInputRef = useRef();
+  const deptInputRef = useRef();
+  const superiorInputRef = useRef();
   return (
     <StyledAddEmployee>
       <h4>Add New Employee</h4>
       <form>
         <div className="employee-input">
-          <input type="text" placeholder="First name" />
-          <input type="text" placeholder="Last Name" />
+          <input type="text" placeholder="First name" ref={nameInputRef} />
+          {/* <input type="text" placeholder="Last Name" /> */}
         </div>
         <div className="employee-input">
-          <input type="text" placeholder="employee title" />
-          <input type="text" placeholder="department" />
-          <input type="text" placeholder="reports to" />
+          <input type="text" placeholder="employee title" ref={titleInputRef} />
+          <input type="text" placeholder="department" ref={deptInputRef} />
+          <input type="text" placeholder="reports to" ref={superiorInputRef} />
         </div>
         <div className="skills-input">
           <form>
