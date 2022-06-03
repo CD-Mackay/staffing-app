3a93248dc499d3c47fc7a4b1c9d94180f48c94f1
Connor Mackay <connor.mackay@gmail.com>
2022-06-03 14:27:23 -0400

    git status

--- src/components/AddEmployee/AddEmployee.js
+++ src/components/AddEmployee/AddEmployee.js
@@ -2,11 +2,25 @@ import React from "react";
 
 import StyledAddEmployee from "./StyledAddEmployee";
 
-export default function AddEmployee(){
+export default function AddEmployee() {
   return (
     <StyledAddEmployee>
-      <p>G'day, I'm addemployee</p>
+      <form>
+        <div className="name-input">
+          <input type="text" placeholder="First name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <div className="role-input">
+          <input type="text" placeholder="employee title" />
+          <input type="text" placeholder="department" />
+          <input type="text" placeholder="reports to" />
+        </div>
+        <div className="skills-input">
+          <form>
+            <input type="text" placeholder="skill" />
+          </form>
+        </div>
+      </form>
     </StyledAddEmployee>
   );
-};
-
+}
