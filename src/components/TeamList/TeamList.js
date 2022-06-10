35d8c7ec1bbfe8e2ad152b33afe4254b30f0ae03
Connor Mackay <connor.mackay@gmail.com>
2022-06-10 14:52:30 -0400

    build method to retrieve team data

--- src/components/TeamList/TeamList.js
+++ src/components/TeamList/TeamList.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import StyledTeamList from "./StyledTeamList";
 
+import { getAllTeams } from "../../utilities/db-helpers";
+
 const TeamList = () => {
-return (
-    <StyledTeamList>
-      
-    </StyledTeamList>
-  );
+
+  const [teams, setTeams] = useState([]);
+  const makeTeamList = async () => {
+    let result = await getAllTeams();
+    setTeams(result);
+  };
+
+  useEffect(() => {
+    makeTeamList();
+  }, []);
+  return <StyledTeamList></StyledTeamList>;
 };
 
 export default TeamList;
--- src/utilities/db-helpers.js
+++ src/utilities/db-helpers.js
@@ -14,6 +14,22 @@ export async function getAllStaff() {
   return staff;
 };
 
+export async function getAllTeams() {
+  const response = await fetch(' https://staffing-app-8610f-default-rtdb.firebaseio.com/staff.json');
+  const data = await response.json();
+
+  const teams = [];
+
+  for (const key in data) {
+    teams.push({
+      id: key,
+      ...data[key]
+    })
+  }
+
+  return teams;
+}
+
 export async function addTeam(object) {
   const response = await fetch(`https://staffing-app-8610f-default-rtdb.firebaseio.com/teams.json`, {
     method: 'POST',
