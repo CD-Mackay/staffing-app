fbe890477d33dd23444e281e4e1a9a780412c5cf
Connor Mackay <connor.mackay@gmail.com>
2022-06-10 14:33:18 -0400

    build form to add employees to team

--- src/components/MakeTeam/MakeTeam.js
+++ src/components/MakeTeam/MakeTeam.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import StyledMakeTeam from "./StyledMakeTeam";
 
 const MakeTeam = ({staff}) => {
+
+  const [teamList, setTeamList] = useState([]);
+  const [selected, setSelected] = useState("");
+
+  const handleAddToTeam = () => {
+    const teamCopy = [...teamList];
+    teamCopy.push(selected);
+    setTeamList(teamCopy);
+  };
+
+  const handleSelectEmployee = (event) =>{
+    event.preventDefault();
+    setSelected(event.target.value);
+
+  }
 return (
     <StyledMakeTeam>
       <form>
         <input type="text" placeholder="team name" />
         <input type="text" placeholder="team lead" />
-        <select name="employees" id="employees">
+        <select name="employees" id="employees" onChange={(e) => handleSelectEmployee(e)}>
           {staff.map((employee) => {
             return <option value={employee.name}>{employee.name}</option>
           })}
         </select>
+        <button onClick={handleAddToTeam}>add to team</button>
       </form>
     </StyledMakeTeam>
   );
