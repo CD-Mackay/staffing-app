9c08d660845ae47f15b44cc086b0aeb563a564d6
Connor Mackay <connor.mackay@gmail.com>
2022-06-20 15:15:26 -0400

    add hover states to flag button

--- src/components/FlagButton/StyledFlagButton.js
+++ src/components/FlagButton/StyledFlagButton.js
@@ -1,17 +1,28 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const StyledFlagButton = styled.div`
-display: flex;
-flex-direction: column;
-button {
-  background: none; 
-  border: none;
-  color: gray;
-}
-button:hover {
-  cursor: pointer;
-  color: black;
-}
+  display: flex;
+  flex-direction: column;
+  button {
+    background: none;
+    border: none;
+  }
+  button:hover {
+    cursor: pointer;
+  }
+
+  .green-button {
+    color: green;
+  }
+  .green-button:hover {
+    color: #bdf0c9;
+  }
+  .red-button {
+    color: red;
+  }
+  .red-button:hover {
+    color: #f0c9cd;
+  }
 `;
 
 export default StyledFlagButton;
