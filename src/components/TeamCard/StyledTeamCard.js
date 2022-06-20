61dedfa9faa84917d374cfa1f4e5abcd292f65d5
Connor Mackay <connor.mackay@gmail.com>
2022-06-20 15:33:17 -0400

    style employee listings on teams

--- src/components/TeamCard/StyledTeamCard.js
+++ src/components/TeamCard/StyledTeamCard.js
@@ -8,6 +8,20 @@ padding: 5px;
 ul {
   list-style-type: none;
 }
+
+.employee-listing {
+  display: flex;
+  max-width: 200px;
+  justify-content: space-between;
+  margin: 10px;
+  div {
+    display: flex;
+    flex-direction: column;
+    button {
+      margin-bottom: 5px;
+    }
+  }
+}
 `;
 
 export default StyledTeamCard;
--- src/components/TeamCard/TeamCard.js
+++ src/components/TeamCard/TeamCard.js
@@ -3,18 +3,31 @@ import React from "react";
 import StyledTeamCard from "./StyledTeamCard";
 
 const TeamCard = ({ name, lead, team }) => {
+  const Listing = ({ name }) => {
+    return (
+      <li>
+        <div className="employee-listing">
+          <p>{name}</p>
+          <div>
+            <button>view employee</button>
+            <button>remove from team</button>
+          </div>
+        </div>
+      </li>
+    );
+  };
   return (
     <StyledTeamCard>
       <h6>
         {name} -- Team Lead: {lead}
       </h6>
       <ul>
-      {team &&
-        team.map((element) => {
-          return <li key={element}>{element}<button>x</button></li>;
-        })}
+        {team &&
+          team.map((element) => {
+            return <Listing name={element} />;
+          })}
         {!team && <li>No members in this team</li>}
-        </ul>
+      </ul>
     </StyledTeamCard>
   );
 };
