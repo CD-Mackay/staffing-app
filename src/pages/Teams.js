720b6e462eb8ae4862f1ace7b52b024fb18dfd5c
Connor Mackay <connor.mackay@gmail.com>
2022-06-22 17:24:05 -0400

    refactor to have state at page level

--- src/pages/Teams.js
+++ src/pages/Teams.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MakeTeam from "../components/MakeTeam/MakeTeam";
 import TeamList from "../components/TeamList/TeamList";
+import { getAllTeams } from "../utilities/db-helpers";
 
 export default function Teams({ staff }) {
+  const [teams, setTeams] = useState([]);
+  const makeTeamList = async () => {
+    let result = await getAllTeams();
+    setTeams(result);
+  };
+
+  useEffect(() => {
+    makeTeamList();
+  }, [teams]);
   return (
     <div>
-      <MakeTeam staff={staff} />
-      <TeamList />
+      <MakeTeam staff={staff} setTeams={setTeams} teams={teams} />
+      <TeamList teams={teams} />
     </div>
   );
 }
